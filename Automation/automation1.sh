f6cb99da96c78e3f20dbec2a0aea21380efd82b1
BhavanaBollampally <bhavanabollampally3@gmail.com>
2025-10-12 18:32:49 +0530

    Add function to create multiple log folders and log files with timestamps

--- Automation/automation1.sh
+++ Automation/automation1.sh
@@ -1,27 +1,36 @@
 #!/usr/bin/bash
 #Author: Bhavana_System
 
-# Create log folder if it doesn't exist
-mkdir -p ~/Desktop/ShellScripting/log_folder
+message () {
+#iterating for craeting folders
+k=1
+#takes the no of folders as $1
+while [ $k -le $1 ]
+do
+#creates the folders
+mkdir -p ~/Desktop/ShellScripting/log_folder_${k}
+#goes to the folder
+cd ~/Desktop/ShellScripting/log_folder_${k}/
 i=1
 while [ $i -le 10 ] 
 do
-cd ~/Desktop/ShellScripting/log_folder/
-
-# Define log file variable
+#creates log file
 log_file="log_${i}.log"
-
-# Get current working directory
+#runs the command and stores in output
 output=$PWD
-
-# Append message with timestamp to the log file
-echo "Message received $output at [$(date)]" >> "$log_file"
-
-# Feedback to user
-echo "Log file $i  created successfully at $log_file"
-
+#output message is stored in log file with date stamp
+echo "Message received $output at [$(date)] " >> "$log_file" 
+#informs user about successfull log file creation
+echo "log file created successfully at [$(date)]" >> log_msg.log
+#after 1 sec creates another log file
 sleep 1
+#logfile 1 to log file2
 ((i++))
 done
-echo "10 logfiles created successfully"
-
+#after 3 sec creates another folder
+sleep 3
+#log_folder1 to log_folder2
+((k++))
+done
+}
+message 5
