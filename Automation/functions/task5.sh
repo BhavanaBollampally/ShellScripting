669daab2f830e7ba8c4757441b3edd5999fe1f79
BhavanaBollampally <bhavanabollampally3@gmail.com>
2025-10-14 11:59:53 +0530

    Added all metrics in task5

--- Automation/functions/task5.sh
+++ Automation/functions/task5.sh
@@ -1,15 +1,31 @@
 #!/usr/bin/bash
 #Author: Bhavana_System
-sytem_metric() {
+system_metric() {
+	#user input as choice
 	local choice="$1"
 	case $choice in
 		cpu)
+			#Cpu Usage
 			local output1=$(top -bn1 | awk '/%Cpu/ {print"Cpu Usage: " (100 - $8) "%"}')
 			echo "$output1"
 			;;
 		memory)
+			#Memory Usage
 			local output2=$(free -h | awk '/Mem:/ {print"Memory Usage: " $3}')
 			echo "$output2"
 			;;
 		disk)
-			local output3=$()
+			#Disk usage
+			local output3=$(df -h | awk '$6=="/" {print "Disk Usage: " $5}')
+			echo "$output3"
+			;;
+		*)
+			echo "Invalid choice! please enter cpu,memory or disk."
+	esac
+}
+	#select choice from user
+	read -p " Enter metric to check (cpu/memory/disk): " i
+	#function run from user input
+	res=$(system_metric "$i")
+	echo "$res"
+
